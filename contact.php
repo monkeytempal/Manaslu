df55680ae934951cd129df648d561a429b59badc
Bijay Pakhrin <monkeytempal@gmail.com>
2014-11-09 09:33:10 +0545

    udpated Contact page

--- contact.php
+++ contact.php
@@ -4,11 +4,13 @@
 	<div id="blogContainer">
 		<div class="container content">
 			<section class="grid_8">
-
+			
+			<!-- Google Map -->
 			<div id="mapContainer">
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.0755026602546!2d85.29034399999996!3d27.714955000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb188d75bc6a8b%3A0x9cc9fe15435db231!2sSwayambhunath+Stupa!5e0!3m2!1sen!2s!4v1415424523826" width="100%" height="350" frameborder="0" style="border:0"></iframe>
-			</div><!-- Google Map -->
+			</div>
 
+			<!-- Business Address -->
 			<div id="addressContainer">
 				<header class="heading">
 					<h2>Our corporate addresses</h2>
@@ -48,13 +50,15 @@
 					</address>
 				</div>
 			</div>
-
+			
+			<!-- Contact Form Wrapper -->
 			<div id="contact-form">
 				<header class="heading">
 					<h2>Send us a message</h2>
 				</header>
 
 				<form>
+					<!-- Form Container -->
 					<div class="form"> 
 						<div class="control-group">
 							<label class="control-label">Name:</label>
@@ -91,9 +95,9 @@
 						<div class="control-group">
 							<input class="btn" value="Send message" type="submit" />
 						</div>
-					</div>	<!-- Form Container Ends -->
-				</form> <!-- Contact Form Ends -->
-			</div> <!-- Contact Form Wrapper Ends -->
+					</div>	
+				</form>
+			</div> 
 
 			</section>
 
